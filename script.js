910021d573921ce528ab9212dbcdc55505ea982e
cornerofrandom <78874121+cornerofrandom@users.noreply.github.com>
2021-02-10 13:29:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+ document.getElementById("changeback").style.display = "none";
+ document.getElementById("changebtn").style.display = "block";
+ document.getElementById("unr").style.display = "none";
+function change(){
+    document.getElementById("changeme").innerHTML = "The paragraph changed.";
+    document.getElementById("changebtn").style.display = "none";
+    document.getElementById("changeback").style.display = "block";
+}
+function changeback(){
+    document.getElementById("changeme").innerHTML = "A paragraph";
+    document.getElementById("changebtn").style.display = "block";
+    document.getElementById("changeback").style.display = "none";
+}
+function unr(){
+    document.getElementById("unr").style.display = "block";
+    document.getElementById("unrb").style.display = "none";
+}
+alert("ATTENTION: you are entering a corner of random. Beware of extreme levels of randomness. You have been warned...")
