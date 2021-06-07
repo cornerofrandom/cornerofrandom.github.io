fa7d9966dc69526661f2ffee9d84def1e29692c2
cornerofrandom <78874121+cornerofrandom@users.noreply.github.com>
2021-06-06 21:46:07 -0400

    Update biocard.js

--- biocard.js
+++ biocard.js
@@ -1,42 +1,24 @@
-var colors = [
+//listen for changes to any of the sections of the bio-card
+document.getElementById("name").addEventListener("input", function() {
+    save();
+}, false);
+document.getElementById("desc").addEventListener("input", function() {
+    save();
+}, false);
+document.getElementById("birthday").addEventListener("input", function() {
+    save();
+}, false);
+document.getElementById("likes").addEventListener("input", function() {
+    save();
+}, false);
+document.getElementById("dislikes").addEventListener("input", function() {
+    save();
+}, false);
+document.getElementById("about").addEventListener("input", function() {
+    save();
+}, false);
 
-    "#FFE4F5", 
-    "#FFDDDD",
-    "#FFE8D8",
-    "#FFFCDD",
-    "#E4FFDD",
-    //"#DDFFF1", 
-    "#D9FFFA",
-    "rgb(201, 240, 253)", 
-    "#DAD9FF", 
-    //"#E8CAFF",
-];
-var colorIndex = 0;
-
-function changeColor() {
-    var col = document.getElementById("print");
-    if( colorIndex >= colors.length ) {
-        colorIndex = 0;
-    }
-    col.style.backgroundColor = colors[colorIndex];
-    colorIndex++;
-}
-
-
-
-function print(){
-
-
-html2canvas(document.querySelector("#print")).then(canvas => {
-    //document.body.appendChild(canvas); //adds the "screenshot" to the webpage, which we don't need
-    var img = canvas.toDataURL("image/png")
-    var win = window.open();
-    win.document.write("<p>you can save this as an image to your desktop. <br> didn't render properly? <del>yeah html2canvas kinda sucks</del> try saving and reloading the biomaker website, then try again. </p><img src='"+img+"'/>")
-});
-}
-
-
-//SAVE/LOAD THE USER'S WORK, USING localStorage
+//check to make sure that localStorage is available
 function storageAvailable(type) {
     var storage;
     try {
@@ -51,12 +33,11 @@ function storageAvailable(type) {
     }
 }
 
-//editableDiv = document.getElementById("edited"); // get the editable div
- // get the editable div
+
 
 function save() { // this function will be executed whenever the content of the editableDiv changes
     if (storageAvailable("localStorage")) {
-        document.getElementById("sStatus").innerHTML="saving.."
+        document.getElementById("sStatus").innerHTML="<span style='color: yellow;'>Saving..</span>"
 
         nameDiv = document.getElementById("name"); //fetch each div and assign it a name
         descDiv = document.getElementById("desc");
@@ -64,24 +45,31 @@ function save() { // this function will be executed whenever the content of the 
         lDiv = document.getElementById("likes");
         dlDiv = document.getElementById("dislikes");
         aboutDiv = document.getElementById("about");
-
+        color = document.getElementById("cardColor");
 
         uname = nameDiv.innerHTML; //get the content of the div
-        window.localStorage.setItem("name", uname); //save the content
-
+            window.localStorage.setItem("name", uname); //save the content
         desc = descDiv.innerHTML;
-        window.localStorage.setItem("desc", desc);
+            window.localStorage.setItem("desc", desc);
         b = bDiv.innerHTML;
-        window.localStorage.setItem("b", b);
+            window.localStorage.setItem("b", b);
         l = lDiv.innerHTML;
-        window.localStorage.setItem("l", l);
+            window.localStorage.setItem("l", l);
         dl = dlDiv.innerHTML;
-        window.localStorage.setItem("dl", dl);
+            window.localStorage.setItem("dl", dl);
         about = aboutDiv.innerHTML;
-        window.localStorage.setItem("about", about);
+            window.localStorage.setItem("about", about);
+        cc = color.innerHTML;
+        window.localStorage.setItem("color", cc);
+
 
         var d = new Date()
-        document.getElementById("sStatus").innerHTML="<span style='color: green'>saved.</span> <br>"+d;
+        var str = d.toString();
+        var short = str.substring(0,34);
+        document.getElementById("sStatus").innerHTML="<span style='color: green'>Saved.</span>";
+        document.getElementById("date").innerHTML=short;
+
+        window.localStorage.setItem("lastSave", short);
     } else { // if localStorage is not supported, notify the user about it
         alert("Error saving: your browser does not support localStorage, or it was disabled.");
     }
@@ -98,6 +86,8 @@ function save() { // this function will be executed whenever the content of the 
         lDiv = document.getElementById("likes");
         dlDiv = document.getElementById("dislikes");
         aboutDiv = document.getElementById("about");
+        color = document.getElementById("print");
+        
 
         uname = window.localStorage.getItem("name");
         desc = window.localStorage.getItem("desc");
@@ -105,10 +95,12 @@ function save() { // this function will be executed whenever the content of the 
         l = window.localStorage.getItem("l");
         dl = window.localStorage.getItem("dl");
         about = window.localStorage.getItem("about");
+        cc = window.localStorage.getItem("color");
 
         if (uname === null) { // if the user never modified the divcontent before (or cleaned his localStorage), do nothing
-            var d = new Date()
-        document.getElementById("sStatus").innerHTML="<span style='color: red'>no saved data.</span> <br>" +d;
+        document.getElementById("sStatus").innerHTML="<span style='color: red'>No saved data.</span>";
+        
+        //document.getElementsByName("date").innerHTML=short;
         } else {
             nameDiv.innerHTML = uname; // write it into the editableDiv
             descDiv.innerHTML = desc;
@@ -116,11 +108,19 @@ function save() { // this function will be executed whenever the content of the 
             lDiv.innerHTML = l;
             dlDiv.innerHTML = dl;
             aboutDiv.innerHTML = about;
+            color.style.backgroundColor = cc;
+
+            p = document.getElementById("cardColor")
+            p.innerHTML = cc;
             var d = new Date()
-        document.getElementById("sStatus").innerHTML="<span style='color: green'>saved data was loaded.</span> <br>" + d;
+            var str = d.toString();
+           // var short = str.substring(0,34);
+        document.getElementById("sStatus").innerHTML="<span style='color: green'>Saved data was loaded.</span>";
+        document.getElementById("date").innerHTML = "Last saved: \n" + window.localStorage.getItem("lastSave");
+        //document.getElementById("date").innerHTML=short;
         }
     } else { // if strorage not available, notify the user
-        alert("local storage is not available on your browser.");
+        alert("Local storage is not available on your browser. Data was not saved.");
     }
  }
 
@@ -137,12 +137,20 @@ function save() { // this function will be executed whenever the content of the 
     }
  }
 
- function ccf(){
-    var col = document.getElementById("print");
-    var colorcode = document.getElementById("ccc");
-    var hex = colorcode.innerHTML;
-    try {col.style.backgroundColor = `${hex}`;}
-    catch {
-        alert('the color you entered was invalid, please try again and make sure you put # before your hex code!')
-    }
- }
+
+
+
+
+  function openNav() {
+    document.getElementById("sidebar").style.visibility = "visible";
+
+
+  }
+  
+  function closeNav() {
+    document.getElementById("sidebar").style.visibility = "hidden";
+  }
+
+  function hideBD(){
+      document.getElementById("boxBD").style.visibility="hidden";
+  }
